0368e624b4cdf343e6abbcc6bb8419621a028960
John Desktop <jdufresne135@gmail.com>
2024-01-10 16:18:49 -0500

    Remove Duplicate code finished

--- LeetCode/Top_Interview_Questions/Array/removeDuplicates.java
+++ LeetCode/Top_Interview_Questions/Array/removeDuplicates.java
@@ -1,19 +1,40 @@
+
 //John Dufresne 01/06/2024
-//Remove Duplicates from an int array thats alreadyb sorted in acsending order.
+//Remove Duplicates from an int array, IN PLACE, thats already been sorted in acsending order.
 //If they were not sorted i would first sort using mergesort then perform my function below.
+import java.util.Arrays; //for testing
+
 public class removeDuplicates {
     public static int removeDuplicates(int[] nums) {
         int count = 0;
-        for(int i = 0; i < nums.length-1; i++){
-            if(nums[i] != nums[i+1])
+        if (nums.length == 1)
+            return 1;
+        else {
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] == -101)
+                    break;
+                if (nums[i] == nums[i + 1]) {
+                    while (nums[i] == nums[i + 1]) {
+                        int j = i+1, n = nums.length - 1;
+                        while (j < n) {
+                            nums[j] = nums[j + 1];
+                            j++;
+                        }
+                        nums[n] = -101;
+                    }
+                }
                 count++;
-            if(i+1 == nums.length)
-            count++;
+            }
         }
+        // System.out.println(Arrays.toString(nums)); //for testing
+        if(nums[nums.length-1] != -101)
+            count++;
         return count;
     }
+
     public static void main(String[] args) {
-        int[] nums= {1,1,1,2,2,2,3,3,3,4,4,4,5,5};
-        System.out.println(removeDuplicates(nums));
+        int[] nums = {1, 1, 2}; // Input array
+        int k = removeDuplicates(nums); // Calls your implementation
+        System.out.println(k);
     }
 }
