f5bc67350aaed7c4cc6603aae8b3115bf4b2edb4
JDufresne12 <JDufresne135@gmail.com>
2024-02-05 22:02:43 -0500

    MoveZeroes Init

--- LeetCode/Top_Interview_Questions/Array/MoveZeroes.java
+++ LeetCode/Top_Interview_Questions/Array/MoveZeroes.java
@@ -6,7 +6,16 @@
 
     Input: nums = [0,1,0,3,12]
     Output: [1,3,12,0,0]
-    
+
     Input: nums = [0]
     Output: [0]
 */ 
+
+public class MoveZeroes{
+    public void moveZeroes(int[] nums) {
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
