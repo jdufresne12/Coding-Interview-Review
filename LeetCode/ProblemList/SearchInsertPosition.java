3dfcbee333fc761e586965ebb23fe815e9463436
jdufresne12 <jdufresne135@gmail.com>
2025-02-19 12:14:33 -0500

    Search

---
+++ LeetCode/ProblemList/SearchInsertPosition.java
@@ -0,0 +1,30 @@
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return low;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1 };
+        int target = 1;
+        SearchInsertPosition solution = new SearchInsertPosition();
+
+        System.out.println(solution.searchInsert(nums, target));
+    }
+}
