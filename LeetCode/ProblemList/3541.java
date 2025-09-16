9f82aa0dd00b145742e879f10a53589e87e09314
jdufresne12 <jdufresne135@gmail.com>
2025-09-16 13:23:54 -0400

    Find Most Frequent Vowel and Consonant

---
+++ LeetCode/ProblemList/3541.java
@@ -0,0 +1,73 @@
+/*
+    3541. Find Most Frequent Vowel and Consonant
+    You are given a string s consisting of lowercase English letters ('a' to 'z').
+
+    Your task is to:
+
+    Find the vowel (one of 'a', 'e', 'i', 'o', or 'u') with the maximum frequency.
+    Find the consonant (all other letters excluding vowels) with the maximum frequency.
+    Return the sum of the two frequencies.
+
+    Note: If multiple vowels or consonants have the same maximum frequency, you may choose any one of them. If there are no vowels or no consonants in the string, consider their frequency as 0.
+
+    The frequency of a letter x is the number of times it occurs in the string.
+
+    Example 1:
+    Input: s = "successes"
+    Output: 6
+    Explanation:
+        The vowels are: 'u' (frequency 1), 'e' (frequency 2). The maximum frequency is 2.
+        The consonants are: 's' (frequency 4), 'c' (frequency 2). The maximum frequency is 4.
+        The output is 2 + 4 = 6.
+
+    Example 2:
+    Input: s = "aeiaeia"
+    Output: 3
+    Explanation:
+        The vowels are: 'a' (frequency 3), 'e' ( frequency 2), 'i' (frequency 2). The maximum frequency is 3.
+        There are no consonants in s. Hence, maximum consonant frequency = 0.
+        The output is 3 + 0 = 3.
+ */
+
+import java.util.*;
+
+class Solution {
+    public int maxFreqSum(String s) {
+        Map<Character, Integer> alphabet = new HashMap<>();
+        int maxVowel = 0;
+        int maxConsonant = 0;
+
+        for (char c : s.toCharArray()) {
+            c = Character.toLowerCase(c);
+            alphabet.put(c, alphabet.getOrDefault(c, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : alphabet.entrySet()) {
+            char letter = entry.getKey();
+            int count = entry.getValue();
+
+            if (isVowel(letter)) {
+                if (count > maxVowel) {
+                    maxVowel = count;
+                }
+            } else {
+                if (count > maxConsonant) {
+                    maxConsonant = count;
+                }
+            }
+        }
+
+        return maxVowel + maxConsonant;
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        String s1 = "aeiaeia";
+        System.out.println(sol.maxFreqSum(s1));
+    }
+}
