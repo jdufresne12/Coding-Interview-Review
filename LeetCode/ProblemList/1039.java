b41f8456a40a4c0f47cbd0e7146c6669846b518a
Jdufresne12 <jdufresne135@gmail.com>
2025-09-29 16:08:43 -0400

    Minimum Score Triangulation of Polygon

---
+++ LeetCode/ProblemList/1039.java
@@ -0,0 +1,58 @@
+/*	
+	You have a convex n-sided polygon where each vertex has an integer value. You are given an integer array values where values[i] is the value of the ith vertex in clockwise order.
+
+	Polygon triangulation is a process where you divide a polygon into a set of triangles and the vertices of each triangle must also be vertices of the original polygon. Note that no other shapes other than triangles are allowed in the division. This process will result in n - 2 triangles.
+
+	You will triangulate the polygon. For each triangle, the weight of that triangle is the product of the values at its vertices. The total score of the triangulation is the sum of these weights over all n - 2 triangles.
+
+	Return the minimum possible score that you can achieve with some triangulation of the polygon.
+
+	Example 1:
+	Input: values = [1,2,3]
+	Output: 6
+	Explanation: The polygon is already triangulated, and the score of the only triangle is 6.
+
+	Example 2:
+	Input: values = [3,7,4,5]
+	Output: 144	
+	Explanation: There are two triangulations, with possible scores: 3*7*5 + 4*5*7 = 245, or 3*4*5 + 3*4*7 = 144.
+	The minimum score is 144.
+
+	Example 3:
+	Input: values = [1,3,1,4,1,5]
+	Output: 13
+	Explanation: The minimum score triangulation is 1*1*3 + 1*1*4 + 1*1*5 + 1*1*1 = 13.
+*/
+
+class Solution {
+    	public int minScoreTriangulation(int[] values) {
+	        int n = values.length;
+		int[][] dp = new int[n][n];
+		
+		for(int gap = 2; gap < n; gap++){
+			for(int i = 0; i + gap < n; i++){
+				int j = i + gap;
+				
+				if(gap == 2) {
+					dp[i][j] = values[i] * values[i + 1] * values[j];
+				} else {
+					dp[i][j] = Integer.MAX_VALUE;
+					
+					for(int k = i + 1; k < j; k++) {
+						int score = (values[i] * values[k] * values[j]) + dp[i][k] + dp[k][j];
+						dp[i][j] = Math.min(dp[i][j], score);
+					}
+				}
+			}
+		}
+		
+		return dp[0][n-1];
+	}
+	
+	public static void main(String[] args){
+		Solution sol = new Solution();
+		
+		int[] values = {1, 2, 3};
+		System.out.print(sol.minScoreTriangulation(values)); 
+	}
+}
