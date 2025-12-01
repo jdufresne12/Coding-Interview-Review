9ada69ac4990b1d3b882605ec6047dcdce60fbdf
jdufresne12 <jdufresne135@gmail.com>
2025-12-01 14:40:43 -0500

    2011. Final value of variable after operations

---
+++ LeetCode/ProblemList/2011.java
@@ -0,0 +1,46 @@
+/*
+    2011. Final value of a variable after performing operations
+
+    There is a programming language with only four operations and one variable X:
+    ++X and X++ increments the value of the variable X by 1.
+    --X and X-- decrements the value of the variable X by 1.
+    Initially, the value of X is 0.
+
+    Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+    Example 1:
+
+    Input: operations = ["--X","X++","X++"]
+    Output: 1
+    Explanation: The operations are performed as follows:
+    Initially, X = 0.
+    --X: X is decremented by 1, X =  0 - 1 = -1.
+    X++: X is incremented by 1, X = -1 + 1 =  0.
+    X++: X is incremented by 1, X =  0 + 1 =  1.
+*/
+class Solution {
+    public int finalValueAfterOperations(String[] operations){
+        int x = 0;
+        for (String s: operations) {
+            if(s.contains("-"))
+                x--;
+            else
+                x++;
+        }
+
+        return x;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        String[] operations = {"--X","X++","X++"};
+        System.out.println(sol.finalValueAfterOperations(operations));
+
+        String[] operations1 = {"++X","++X","X++"};
+        System.out.println(sol.finalValueAfterOperations(operations1));
+
+        String[] operations2 = {"X++","++X","--X","X--"};
+        System.out.println(sol.finalValueAfterOperations(operations2));
+    }
+}
