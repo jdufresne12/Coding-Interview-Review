8c233e38dce444fdd673b01b82cbd2753e10c8ea
jdufresne12 <jdufresne135@gmail.com>
2026-03-06 12:18:14 -0500

    1784

---
+++ LeetCode/ProblemList/1784.java
@@ -0,0 +1,16 @@
+/*
+ * 1784. Check if Binary String has at most one segment of Ones 
+ * Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. Otherwise, return false.
+ */
+
+class Solution {
+    public boolean checkOnesSegment(String s) {
+
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s = "1001";
+        Sout
+    }
+}
